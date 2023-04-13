1ebce6b93574d788db872b02a18649f976fb7d1d
Scott Robinson <scott.robinson2@sky.uk>
2023-04-13 11:39:11 +0100

    Complete base task to specifications

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-const express = require('express');
+const jwt = require("jsonwebtoken");
+const express = require("express");
 const app = express();
-const { User } = require('./db');
+const { User, Kitten } = require("./db");
 
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
+
+const { JWT_SECRET = "super-secret-key" } = process.env;
 
 app.get("/", async (req, res, next) => {
     try {
@@ -21,6 +24,46 @@ app.get("/", async (req, res, next) => {
 
 // Verifies token with jwt.verify and sets req.user
 // TODO - Create authentication middleware
+async function authMW(req, resp, next) {
+    const auth = req.get("Authorization");
+    if (!auth) {
+        resp.sendStatus(401);
+        return;
+    }
+
+    const [, token] = auth.split(" ");
+    const user = jwt.verify(token, JWT_SECRET);
+
+    req.user = user;
+    next();
+}
+
+async function getKittenMW(req, resp, next) {
+    if (!req.user) {
+        resp.status(500).send("getKittenMW can only be used after the authMW");
+        return;
+    }
+
+    const kittenId = req.params.id;
+    if (kittenId === undefined) {
+        resp.sendStatus(400);
+        return;
+    }
+
+    const kitten = await Kitten.findByPk(kittenId);
+    if (!kitten) {
+        resp.sendStatus(404);
+        return;
+    }
+
+    if (kitten.ownerId !== req.user.id) {
+        resp.sendStatus(401);
+        return;
+    }
+
+    req.kitten = kitten;
+    next();
+}
 
 // POST /register
 // OPTIONAL - takes req.body of {username, password} and creates a new user with the hashed password
@@ -30,12 +73,30 @@ app.get("/", async (req, res, next) => {
 
 // GET /kittens/:id
 // TODO - takes an id and returns the cat with that id
+app.get("/kittens/:id", authMW, getKittenMW, async (req, resp) => {
+    resp.json({ name: req.kitten.name, age: req.kitten.age, color: req.kitten.color });
+});
 
 // POST /kittens
 // TODO - takes req.body of {name, age, color} and creates a new cat with the given name, age, and color
+app.post("/kittens", authMW, async (req, resp) => {
+    const { name, age, color } = req.body;
+    // We need an explicit check on age since could be 0 which is falsy
+    if (!(name && age !== undefined && color)) {
+        resp.sendStatus(400);
+    }
+
+    const ownerId = req.user.id;
+    await Kitten.create({ name, age, color, ownerId });
+    resp.status(201).json({ name, age, color });
+});
 
 // DELETE /kittens/:id
 // TODO - takes an id and deletes the cat with that id
+app.delete("/kittens/:id", authMW, getKittenMW, async (req, resp) => {
+    await req.kitten.destroy();
+    resp.sendStatus(204);
+});
 
 // error handling middleware, so failed tests receive them
 app.use((error, _, res, __) => {
